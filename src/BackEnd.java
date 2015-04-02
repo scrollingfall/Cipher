b36a5d8f4d66e0880593b438ffe36d2105c0f769
scrollingfall <strangejerrychen@gmail.com>
2015-04-02 16:15:37 -0500

    BackEnd v1

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,15 +1,63 @@
 import java.util.*;
-
+import java.io.*;
 public class BackEnd {
-
+	private ArrayList<String>usernames=new ArrayList<String>();
+	private ArrayList<Encoder>encoders=new ArrayList<Encoder>();
+	private Map<String, Integer>filenamerepeats=new HashMap<String,Integer>();
+	public BackEnd() throws Exception
+	{
+		Scanner scan=new Scanner(new File("Usernames.txt"));
+		while(scan.hasNextLine())
+		{
+			String s=scan.nextLine();
+			if(!usernames.contains(s))
+			{
+				usernames.add(s);
+				String two=new String(s.toCharArray());
+				two.replaceAll("[\\/:\\*?\"<>|]","");
+				if(filenamerepeats.containsKey(two))
+				{
+					filenamerepeats.put(two,filenamerepeats.get(two)+1);
+				}
+				else
+				{
+					filenamerepeats.put(two, 0);
+				}
+				encoders.add(new Encoder(two+"("+filenamerepeats.get(two)+").txt"));
+			}
+		}
+		scan.close();
+	}
 	public List<String> getMessages(String user){
-		return null;
+		if(usernames.contains(user))
+			return encoders.get(usernames.indexOf(user)).read();
+		else
+			return null;
 	}
-	public void addMessage(String m){
-		
+	public void addMessage(String name, String message)throws Exception{
+		if(usernames.contains(name))
+		{
+			encoders.get(usernames.indexOf(name)).write(message);
+		}
+		else
+		{
+			usernames.add(name);
+			String two=new String(name.toCharArray());
+			two.replaceAll("[\\/:\\*?\"<>|]","");
+			if(filenamerepeats.containsKey(two))
+			{
+				filenamerepeats.put(two,filenamerepeats.get(two)+1);
+			}
+			else
+			{
+				filenamerepeats.put(two, 0);
+			}
+			encoders.add(new Encoder(two+"("+filenamerepeats.get(two)+").txt"));
+		}
 	}
-	
 	public List<String> getUsers(){
-		return null;
+		List<String>newlist=new ArrayList<String>(usernames);
+		Collections.sort(newlist);
+		return newlist;
 	}
 }
