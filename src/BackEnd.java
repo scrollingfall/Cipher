fa78da32d69d87b70909b65fe916c65b9e2039ce
scrollingfall <strangejerrychen@gmail.com>
2015-04-02 15:15:30 -0500

    Setup2

---
+++ src/BackEnd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class BackEnd {
+
+	public List<String> getMessages(String user){
+		return null;
+	}
+	public void addMessage(String m){
+		
+	}
+	
+	public List<String> getUsers(){
+		return null;
+	}
+}
---
+++ src/Encoder.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Encoder {
+
+	public Encoder(String f){
+		
+	}
+	public void write(){
+		
+	}
+	public List<String> read(){
+		return null;
+	}
+}
